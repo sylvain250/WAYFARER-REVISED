c842d4e396c29fa57341df4410bec6094582d760
sylvain250 <sylvain.ntezimana21@gmail.com>
2020-01-12 16:18:11 +0200

    ft(API-spin express): To spin express

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // this file is for spinning the express
+
 import mongoose from 'mongoose';
 import express from 'express';
 import employeeRoutes from './routes/employee';
-import errorHandler from './middleware/errorhandler';
+import errorHandler from './middleware/errorhandler'
 
 const app = express();
 import bodyParser from 'body-parser';
 mongoose.connect('mongodb://localhost/Challenge#3')
 .then(()=>console.log('connected to mongoDB...'))
 .catch(err => console.error('Could not connect to mongoDB', err));
+
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
